cb45c2d637a7fab89610f9a5fc73ecab7912f6f4
Amit Chaudhary <160538905+ARC8286@users.noreply.github.com>
2026-06-28 15:39:34 +0530

    removed important data

--- client1/src/services/api.js
+++ client1/src/services/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://arcxzone-webapp.onrender.com/api';
  const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://arcxzone-webapp-production.up.railway.app/api';
-console.log(API_BASE_URL)
 
 const api = axios.create({
   baseURL: API_BASE_URL,
